1b420b3f7abd3f95f8428c70a55b6a4481674cb7
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-12-21 21:05:59 -0500

    updated

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = (app) =>
+{
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  );
+  {
+    res.sendFile('./index.html');
+  });
+};
+
+  
