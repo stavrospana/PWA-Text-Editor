5bcd77e9d08543a70f6ec63ba4f61b4ca30e7674
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-12-21 20:57:53 -0500

    finished project

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+  openDB('jate', 1,
+  {
+    upgrade(db)
+    {
+      if (db.objectStoreNames.contains('jate'))
+      {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', {keyPath: 'id', autoIncrement: true});
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (textEditorContent) =>
+{
+  const jateDB = await openDB('jate', 1); //initializes a connection to the JATE database under version 1
+  const transaction = jateDB.transaction('jate', 'readwrite'); //creates a new transaction instance with the JATE database with read-write privileges
+  const store = transaction.objectStore('jate'); //connects to the JATE object store with above specified conditions
+  const request = store.put({jate: textEditorContent}); //updates text value stored in the indexedDB JATE object store
+
+  //logs updated content to console
+  const savedText = await request;
+  console.log('saved text to database;', savedText);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>
+{
+  const jateDB = await openDB('jate', 1); //initializes a connection to the JATE database under version 1 of the database
+  const transaction = jateDB.transaction('jate', 'readonly'); //creates a read only transaction instance with the JATE database
+  const store = transaction.objectStore('jate'); //connects to the JATE object store with above specified conditions
+  const request = store.get('jate'); //retrieves text from the indexedDB JATE object store
 
-initdb();
+  //logs retrieved content to console
+  const retrievedText = await request;
+  console.log('retrieved text from database;', retrievedText);
+
+  return retrievedText; //returns text retrieved from database
+}
+
+initdb(); //initializes the JATE database
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let installPrompt; //creates an 'installPrompt' variable, which will be used to install the PWA
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) =>
+{
+  console.log('beforeinstallprompt event fired:', event);
+  installPrompt = event; //assigns the beforeinstallprompt event to the 'installPrompt' variable
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () =>
+{
+  //when the install button is clicked, open the PWA installation prompt
+  console.log('install button clicked');
+  installPrompt.prompt();
+
+  //await the user's response to the prompt
+  const promptResponse = await installPrompt.userChoice;
+
+  //if the user accepts the installation request, install the PWA, disable the install button, & clear the installPrompt variable
+  if (promptResponse.outcome === 'accepted')
+  {
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) =>
+{
+  //log that the app was successfully installed after the PWA is downloaded
+  console.log('App Installed;', event);
+});
